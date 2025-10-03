c4dc02c26df9278c3be76cd11100e72ed1a17e5d
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 02:40:04 +0530

    Update and rename commands.py to start.py

---
+++ modules/start.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from bot import app
+
+@app.on_message(filters.command("help") & filters.private)
+async def help_command(client: Client, message: Message):
+    await message.reply_text(
+        f"""This text is from help command
+        ------->
+""",
+        reply_to_message_id=message.id
+    )
+
+
+@Client.on_message(filters.command('total'))
+async def total(bot, message):
+    if message.from_user.id not in OWNER:
+        return await message.reply_text("Who the hell are you!!")
+    msg = await message.reply("Counting total messages in DB...", quote=True)
+    try:
+        total = await Data.count_documents()
+        await msg.edit(f'Total Messages: {total}')
+    except Exception as e:
+        await msg.edit(f'Error: {e}')
