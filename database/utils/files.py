359e814e6298de422a06c8531f29d03ef2d56188
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-03 21:40:58 +0530

    Update files.py

--- database/utils/files.py
+++ database/utils/files.py
@@ -9,53 +9,44 @@ from database import db, instance
 from config import COLLECTION_NAME
 
 @instance.register
-class Media(Document):
-    file_id = fields.StrField(attribute='_id', required=True)
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    mime_type = fields.StrField(allow_none=True)
+class Data(Document):
+    id = fields.StrField(attribute='_id')
+    use = fields.StrField(required=True)
     caption = fields.StrField(allow_none=True)
-    file_type = fields.StrField(allow_none=True)
-
+    file_type = fields.StrField(required=True)
+    channel_id = fields.StrField(allow_none=True)
+    message_id = fields.StrField(allow_none=True)
     class Meta:
-        indexes = ('$file_name',)
         collection_name = COLLECTION_NAME
 
-
-async def get_files_db_size():
+async def save_data(id, caption, file_type, channel_id, message_id):
     try:
-        stats = await db.command("dbstats")
-        return stats['dataSize']
-    except Exception:
-        return 0
-
-
-async def save_file(media):
-    file_id, file_ref = unpack_new_file_id(media.file_id)
-    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-
-    try:
-        file = Media(
-            file_id=file_id,
-            file_ref=file_ref,
-            file_name=file_name,
-            file_size=media.file_size,
-            mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
-            file_type=media.mime_type.split('/')[0] if media.mime_type else None
+        data = Data(
+            id=id,
+            use = "forward",
+            caption=caption,
+            file_type=file_type,
+            channel_id=channel_id,
+            message_id=message_id
         )
-        await file.commit()
-        return 'suc'
-
     except ValidationError:
-        return 'err'
+        logger.exception('Error occurred while saving file in database')
+    else:
+        try:
+            await data.commit()
+        except DuplicateKeyError:
+            logger.warning("Already saved in Database")
+        else:
+            logger.info("Messsage saved in DB")
 
-    except DuplicateKeyError:
-        return 'dup'
 
-    except Exception:
-        return 'err'
+async def get_search_results():
+    filter = {'use': "forward"}
+    cursor = Data.find(filter)
+    cursor.sort('$natural', 1)
+    cursor.skip(0).limit(1)
+    Messages = await cursor.to_list(length=1)
+    return Messages
 
 
 async def search_files(query, max_results=8, offset=0, lang=None):  
