6bded15d41f3130bcfa63ee2d77f21e1f077dd92
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-03 21:21:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv()
 # API credentials
 API_ID = int(getenv("API_ID", "29960871"))
 API_HASH = getenv("API_HASH", "d00a58893f62b37639c687fad05c805c")
-BOT_TOKEN = getenv("BOT_TOKEN", "6658841062:AAGiWmocc3T3trwvhDm6jxheh1X0Y-hcwHE")
+BOT_TOKEN = "5873965762:AAEiw1PiXvIo-DnM9-nEu8ZiPKIWxNH11Ng"
+#getenv("BOT_TOKEN", "6658841062:AAGiWmocc3T3trwvhDm6jxheh1X0Y-hcwHE")
 
 # Owner and logger details
 OWNER_ID = int(getenv("OWNER_ID", "5326801541"))
