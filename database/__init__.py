df470f4f613a4cfb3e8b6aff81382b7cd3d7f2ff
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2025-10-03 16:40:37 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,11 @@
+from umongo.frameworks.motor_asyncio import MotorAsyncIOInstance
+from motor.motor_asyncio import AsyncIOMotorClient
+import config
+
+# Asynchronous Database Connection
+ForwardDB = AsyncIOMotorClient(config.MONGO_URL)
+
+# Database
+db = ForwardDB["ForwardDB"]
+instance = MotorAsyncIOInstance(db)
 
