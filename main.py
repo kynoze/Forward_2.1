fa94c9321a1268417b1908362896ca7fdcd27f2d
thelx0980 <133576014+thelx0980@users.noreply.github.com>
2025-10-03 16:34:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-from bot import Bot
-Bot().run()
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle, errors
+from pyrogram.enums import ChatMemberStatus
+
+from src import app, config
+from src.modules import ALL_MODULES
+from src.logging import LOGGER
+
+
+async def boot():
+    LOGGER(__name__).info("Bot is starting...")
+    await app.start()
+    LOGGER(__name__).info("Bot started successfully.")
+
+    for module in ALL_MODULES:
+        importlib.import_module(f"modules.{module}")
+
+    try:
+        await idle()
+    finally:
+        LOGGER(__name__).warning("Bot is shutting down...")
+        await app.stop()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(boot())
+    except KeyboardInterrupt:
+        LOGGER(__name__).warning("Bot interrupted by user or system.")
