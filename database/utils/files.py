090de7b461178318447bed8a9833694563826b63
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 03:04:57 +0530

    Update files.py

--- database/utils/files.py
+++ database/utils/files.py
@@ -10,36 +10,45 @@ from config import COLLECTION_NAME
 
 
 @instance.register
-class Data(Document):
-    id = fields.StrField(attribute='_id')
-    use = fields.StrField(required=True)
+class Media(Document):
+    file_id = fields.StrField(attribute='_id', required=True)
+    file_ref = fields.StrField(allow_none=True)
+    file_name = fields.StrField(required=True)
+    file_size = fields.IntField(required=True)
+    mime_type = fields.StrField(allow_none=True)
     caption = fields.StrField(allow_none=True)
-    file_type = fields.StrField(required=True)
-    channel_id = fields.StrField(allow_none=True)
-    message_id = fields.StrField(allow_none=True)
+    file_type = fields.StrField(allow_none=True)
+
     class Meta:
+        indexes = ('$file_name',)
         collection_name = COLLECTION_NAME
 
-async def save_data(id, caption, file_type, channel_id, message_id):
-    try:
-        data = Data(
-            id=id,
-            use = "forward",
-            caption=caption,
-            file_type=file_type,
-            channel_id=channel_id,
-            message_id=message_id
-        )
-    except ValidationError:
-        logger.exception('Error occurred while saving file in database')
-    else:
-        try:
-            await data.commit()
-        except DuplicateKeyError:
-            logger.warning("Already saved in Database")
-        else:
-            logger.info("Messsage saved in DB")
 
+async def save_file(media):
+    file_id, file_ref = unpack_new_file_id(media.file_id)
+    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+
+    try:
+        file = Media(
+            file_id=file_id,
+            file_ref=file_ref,
+            file_name=file_name,
+            file_size=media.file_size,
+            mime_type=media.mime_type,
+            caption=media.caption.html if media.caption else None,
+            file_type=media.mime_type.split('/')[0] if media.mime_type else None
+        )
+        await file.commit()
+        return 'suc'
+
+    except ValidationError:
+        return 'err'
+
+    except DuplicateKeyError:
+        return 'dup'
+
+    except Exception:
+        return 'err'
 
 async def get_search_results():
     filter = {'use': "forward"}
