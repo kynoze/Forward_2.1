8302279d64b45f51e9b816189f2ec0a7cba716cd
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-03 20:11:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # API credentials
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 
 # Owner and logger details
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("OWNER_ID", ""))
 
 # MongoDB configuration
 MONGO_URL = getenv("MONGO_URL")
