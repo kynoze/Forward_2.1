2b33baac5afc9600be033c645af65c8032ded0e3
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 11:45:07 +0530

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from bot import app
+from config import OWNER_ID
 from database.utils import Data
+from database import  get_chat, add_chat
 
 @app.on_message(filters.command("help") & filters.private)
 async def help_command(client: Client, message: Message):
@@ -27,8 +29,9 @@ async def total(bot, message):
 
 @app.on_message(filters.private & filters.command(['set_channel']))
 async def set_target_channel(bot, message):    
-    #if Config.ADMINS and not ((str(message.from_user.id) in Config.ADMINS) or (message.from_user.username in Config.ADMINS)):
+    #if OWNER_ID  not ((str(message.from_user.id) in Config.ADMINS) or (message.from_user.username in Config.ADMINS)):
         #return await message.reply("You Are Not Allowed To Use This UserBot")
+    
     try:
         _, chat_id = message.text.split(" ")
     except:
@@ -42,5 +45,5 @@ async def set_target_channel(bot, message):
         chat = await bot.get_chat(chat_id)
     except:
         return await message.reply("Make me a admin in your target channel.")
-    CHANNEL[message.from_user.id] = int(chat.id)
+    add_chat(int(chat.id))
     await message.reply(f"Successfully set {chat.title} target channel.")
