7e859d038cb620447cd2bf29f6974265cd2d78c8
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 10:32:25 +0530

    Update files.py

--- database/utils/files.py
+++ database/utils/files.py
@@ -12,13 +12,9 @@ from config import COLLECTION_NAME
 @instance.register
 class Media(Document):
     file_id = fields.StrField(attribute='_id', required=True)
-    file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    mime_type = fields.StrField(allow_none=True)
     caption = fields.StrField(allow_none=True)
-    file_type = fields.StrField(allow_none=True)
-
+    
     class Meta:
         indexes = ('$file_name',)
         collection_name = COLLECTION_NAME
@@ -32,12 +28,8 @@ async def save_file(media):
         file = Media(
             use='forward'
             file_id=file_id,
-            file_ref=file_ref,
             file_name=file_name,
-            file_size=media.file_size,
-            mime_type=media.mime_type,
             caption=media.caption.html if media.caption else None,
-            file_type=media.mime_type.split('/')[0] if media.mime_type else None
         )
         await file.commit()
         return 'suc'
