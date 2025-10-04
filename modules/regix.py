aab428f2c59454a90d2df493253effa5d654cfc2
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-04 10:46:22 +0530

    Update regix.py

--- modules/regix.py
+++ modules/regix.py
@@ -7,50 +7,18 @@ import logging
 import random
 
 
-async def copy_msg(msg, bot, message):
-   chat_id = msg.chat_id
-   file_id = msg.file_id
-   caption = msg.caption
-   try:                               
-     if msg.get("media") and msg.get("caption"):
-        await bot.send_cached_media(
-              chat_id=sts.get('TO'),
-              file_id=msg.get("media"),
-              caption=msg.get("caption"),
-              reply_markup=msg.get('button'),
-              protect_content=msg.get("protect"))
-     else:
-        await bot.copy_message(
-              chat_id=sts.get('TO'),
-              from_chat_id=sts.get('FROM'),    
-              caption=msg.get("caption"),
-              message_id=msg.get("msg_id"),
-              reply_markup=msg.get('button'),
-              protect_content=msg.get("protect"))
+async def copy_msg(msg, bot, message, chat_id):
+   try:
+     await bot.send_cached_media(
+         chat_id=sts.get('TO'),
+         file_id=msg.file_id,
+         caption=msg.caption)
    except FloodWait as e:
-     await edit(user, m, 'ᴘʀᴏɢʀᴇssɪɴɢ', e.value, sts)
-     await asyncio.sleep(e.value)
-     await edit(user, m, 'ᴘʀᴏɢʀᴇssɪɴɢ', 5, sts)
-     await copy(user, bot, msg, m, sts)
+     await asyncio.sleep(e.value) 
+     await copy_msg(msg, bot, message, chat_id)
    except Exception as e:
      print(e)
-
-def custom_caption(msg, caption):
-  if msg.media:
-    if (msg.video or msg.document or msg.audio or msg.photo):
-      media = getattr(msg, msg.media.value, None)
-      if media:
-        file_name = getattr(media, 'file_name', '')
-        file_size = getattr(media, 'file_size', '')
-        fcaption = getattr(msg, 'caption', '')
-        if fcaption:
-          fcaption = fcaption.html
-        if caption:
-          return caption.format(filename=file_name, size=get_size(file_size), caption=fcaption)
-        return fcaption
-  return None
-
-
+       
 def media(msg):
   if msg.media:
      media = getattr(msg, msg.media.value, None)
