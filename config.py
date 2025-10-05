3f27d27d97ba1a37c0e3e00edf3e4e2e8d4361d0
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-06 02:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "29960871"))
-API_HASH = getenv("API_HASH", "d00a58893f62b37639c687fad05c805c")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "")
-OWNER_ID = set(int(x) for x in getenv("OWNER_ID", "5326801541").split())
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://autopost763:OMnkmZFLXUu0lgnQ@cluster0.wcxqdzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = set(int(x) for x in getenv("OWNER_ID", "").split())
+MONGO_URL = getenv("MONGO_URL", "")
 CACHE_TIME = 300 #int(environ["CACHE_TIME"])
 COLLECTION_NAME = getenv("COLLECTION_NAME", "forward2025")
-TO_CHANNEL = -1001592628992
