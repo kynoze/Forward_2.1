ac56e37eb45982faa34f40e2c35e24d82a5f4ab1
lx0980 <138602811+lx0980@users.noreply.github.com>
2025-10-05 19:50:06 +0530

    Update regix.py

--- modules/regix.py
+++ modules/regix.py
@@ -1,34 +1,23 @@
-import asyncio
 import logging
 from pyrogram.errors import FloodWait
-from database.utils import Media
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-async def copy_msg(msg, bot, message, chat_id, m, message_count):
-    max_attempts = 5
-    attempt = 0
-    while attempt < max_attempts:
-        try:
-            await bot.send_cached_media(
-                chat_id=int(chat_id),
-                file_id=msg.file_id,
-                caption=msg.caption
-            )
-            return True
-        except FloodWait as e:
-            await m.edit_text(
-                f"Total Forwarded: <code>{message_count}</code>\n"
-                f"Sleeping for <code>{e.value}</code> seconds"
-            )
-            await asyncio.sleep(e.value)
-            attempt += 1
-        except Exception as e:
-            logger.exception("Failed to copy message")
-            return False
-    logger.error("Max attempts reached for FloodWait in copy_msg")
-    return False
+async def copy_msg(msg, bot, message, chat_id):
+    try:
+        await bot.send_cached_media(
+            chat_id=int(chat_id),
+            file_id=msg.file_id,
+            caption=msg.caption
+        )
+        return True, None
+    except FloodWait as e:
+        logger.warning(f"FloodWait encountered: {e.value} seconds")
+        return False, e.value
+    except Exception as e:
+        logger.exception("Failed to copy message")
+        return False, None
 
 async def delete_data(data):
     try:
