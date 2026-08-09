e65c3fcb3d87cd71f41943c514f9c1ed484783ad
kynoze <138602811+kynoze@users.noreply.github.com>
2026-08-09 23:06:34 +0530

    Update other_cmd.py

--- modules/other_cmd.py
+++ modules/other_cmd.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 
-CLEAN_FILE_NAME= {}
+CLEAN_FILE_NAME = {}
+CUSTOM_CAPTION_TEXT = {}
 
 @Client.on_message(filters.private & filters.command(['clean_name']))
 async def toggle_clean_name(bot, message):
@@ -11,3 +12,20 @@ async def toggle_clean_name(bot, message):
     CLEAN_FILE_NAME[user_id] = (parts[1].lower() == "on")
     await message.reply(f"✅ CLEAN FILE NAME is now <b>{'ENABLED' if CLEAN_FILE_NAME[user_id] else 'DISABLED'}</b>")
    
+
+@Client.on_message(filters.private & filters.command(['add_caption']))
+async def add_custom_caption(bot, message):
+
+    user_id = message.from_user.id
+
+    try:
+        caption = message.text.split(" ", 1)[1]
+    except:
+        return await message.reply(
+            "Usage:\n/add_caption your text"
+        )
+
+    CUSTOM_CAPTION_TEXT[user_id] = caption
+
+    await message.reply("✅ Custom Caption Saved")
+    
